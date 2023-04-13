d1fa42edba373e40d63df364d021e1a87aeead6c
Lucas Pironet <lucas_pironet@outlook.fr>
2023-04-13 17:54:22 +0200

    mise en place des squelettes nécessaires à l'exploitation des requêtes GET (obtention et utilisation des parametres de la requète) et POST (utilisation d'un body et modification de la DB en conséquence), les requêtes PUT et DELETE fonctionnent similairement

--- backend/routes/anamneses.js
+++ backend/routes/anamneses.js
@@ -1,16 +1,29 @@
 var express = require('express');
+const database = require("../dbPool");
 var router = express.Router();
 
 
-router.get('/', function(req, res, next) {
+router.get('/', async function(req, res, next) {
+  try {
+    const result = await database.pool.query(`select contenu from table_test where ID = ${req.query.clientID}`);
+    valeur = result
+    res.json({ valeur });
+  } catch (err) {
+    throw err;
+  }
+  //res.send(`${req.query.clientID}`);
+});
+
+router.post('/', async function(req, res, next) {
+  try {
+    const result = await database.pool.query(`insert into main_db.tableanamneses(clientID, contenu) values(${req.query.clientID}, "${req.body.contenuBody}") `);
+  } catch (err) {
+    throw err;
+  }
   res.send(`${req.query.clientID}`);
 });
 
-router.post('/', function(req, res, next) {
-  res.send(`${req.query.clientNom}`);
-});
-
-router.put('/', function(req, res, next) {
+router.put('/', async function(req, res, next) {
   res.send(`${req.query.anamneseID}`);
 });
 
