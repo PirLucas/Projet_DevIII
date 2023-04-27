18409a08e46fa6ce1a91f4e5d52c261be2f2b508
Lucas Pironet <lucas_pironet@outlook.fr>
2023-04-27 10:06:23 +0200

    ajout de l'us témoignages et réorganisation en structure react correcte

--- backend/routes/rendezvous.js
+++ backend/routes/rendezvous.js
@@ -1,13 +1,34 @@
 var express = require('express');
 var router = express.Router();
+const database = require("../dbPool");
+
+router.get('/', async function(req, res, next) {
+
+  console.log(Object.keys(req.query)[0])
+  if(Object.keys(req.query)[0] == "clientID") {
+    try {
+
+      const result = await database.pool.query(`select * from tablerdv where rdvID = ${req.query.clientID}`);
+      valeur = result
+      res.json({valeur});
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  else if(Object.keys(req.query)[0] == "rdvDate") {
+    try {
 
 
-router.get('/', function(req, res, next) {
-  res.send(`${req.query.clientID}`);
-});
-
-router.get('/', function(req, res, next) {
-  res.send(`${req.query.rdvDate}`);
+// Display the result
+      console.log(req.query.rdvDate)
+      const result = await database.pool.query(`select rdvDate from tablerdv`);
+      valeur = result
+      console.log(result)
+      res.json({valeur});
+    } catch (err) {
+      console.log(err);
+    }
+  }
 });
 
 router.post('/', function(req, res, next) {
